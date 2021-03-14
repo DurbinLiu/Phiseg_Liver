f2d2e656ccf9de5ebfde9a312069bdd5d314aac1
DurbinLiu <66539992+DurbinLiu@users.noreply.github.com>
2021-03-14 10:35:26 +0800

    Add files via upload
    commit experiments

---
+++ phiseg/experiments/phiseg_7_5.py
@@ -0,0 +1,62 @@
+from phiseg.model_zoo import likelihoods, posteriors, priors
+import tensorflow as tf
+from tfwrapper import normalisation as tfnorm
+
+experiment_name = 'phiseg_7_5'
+log_dir_name = 'lidc'
+
+# architecture
+posterior = posteriors.phiseg
+likelihood = likelihoods.phiseg
+prior = priors.phiseg
+layer_norm = tfnorm.batch_norm
+use_logistic_transform = False
+
+latent_levels = 5
+resolution_levels = 7
+n0 = 32
+zdim0 = 2
+max_channel_power = 4  # max number of channels will be n0*2**max_channel_power
+
+# Data settings
+data_identifier = 'lidc'
+# preproc_folder = '/srv/glusterfs/baumgach/preproc_data/lidc'
+# data_root = '/itet-stor/baumgach/bmicdatasets-originals/Originals/LIDC-IDRI/data_lidc.pickle'
+
+# todo 修改源数据和预处理数据的路径
+preproc_folder = 'E:\\bishe_durbin\\46-PHiSeg-code-master-paper355\data\preproc_data\lidc'
+data_root = 'E:\\bishe_durbin\data_lidc.pickle'
+
+dimensionality_mode = '2D'
+image_size = (128, 128, 1)
+nlabels = 2
+num_labels_per_subject = 4
+
+augmentation_options = {'do_flip_lr': True,
+                        'do_flip_ud': True,
+                        'do_rotations': True,
+                        'do_scaleaug': True,
+                        'nlabels': nlabels}
+
+# training
+optimizer = tf.train.AdamOptimizer
+lr_schedule_dict = {0: 1e-3}
+deep_supervision = True
+batch_size = 6  # 原来：12
+num_iter = 500000 #原来：5000000
+annotator_range = range(num_labels_per_subject)  # which annotators to actually use for training
+
+# losses
+KL_divergence_loss_weight = 1.0
+exponential_weighting = True
+
+residual_multinoulli_loss_weight = 1.0
+
+# monitoring
+do_image_summaries = True
+rescale_RGB = False
+validation_frequency = 500
+validation_samples = 16
+num_validation_images = 100 #'all'
+tensorboard_update_frequency = 100
+
---
+++ phiseg/experiments/phiseg_7_5_1annot.py
@@ -0,0 +1,57 @@
+from phiseg.model_zoo import likelihoods, posteriors, priors
+import tensorflow as tf
+from tfwrapper import normalisation as tfnorm
+
+experiment_name = 'phiseg_7_5_1annot'
+log_dir_name = 'lidc'
+
+# architecture
+posterior = posteriors.phiseg
+likelihood = likelihoods.phiseg
+prior = priors.phiseg
+layer_norm = tfnorm.batch_norm
+use_logistic_transform = False
+
+latent_levels = 5
+resolution_levels = 7
+n0 = 32
+zdim0 = 2
+max_channel_power = 4  # max number of channels will be n0*2**max_channel_power
+
+# Data settings
+data_identifier = 'lidc'
+preproc_folder = '/srv/glusterfs/baumgach/preproc_data/lidc'
+data_root = '/itet-stor/baumgach/bmicdatasets-originals/Originals/LIDC-IDRI/data_lidc.pickle'
+dimensionality_mode = '2D'
+image_size = (128, 128, 1)
+nlabels = 2
+num_labels_per_subject = 4
+
+augmentation_options = {'do_flip_lr': True,
+                        'do_flip_ud': True,
+                        'do_rotations': True,
+                        'do_scaleaug': True,
+                        'nlabels': nlabels}
+
+# training
+optimizer = tf.train.AdamOptimizer
+lr_schedule_dict = {0: 1e-3}
+deep_supervision = True
+batch_size = 12
+num_iter = 5000000
+annotator_range = [0]  # which annotators to actually use for training
+
+# losses
+KL_divergence_loss_weight = 1.0
+exponential_weighting = True
+
+residual_multinoulli_loss_weight = 1.0
+
+# monitoring
+do_image_summaries = True
+rescale_RGB = False
+validation_frequency = 500
+validation_samples = 16
+num_validation_images = 100 #'all'
+tensorboard_update_frequency = 100
+
