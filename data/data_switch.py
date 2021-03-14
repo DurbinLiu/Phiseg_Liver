2475a107dca58e15032423d65c23445b9f185b9f
DurbinLiu <66539992+DurbinLiu@users.noreply.github.com>
2021-03-14 10:33:54 +0800

    Add files via upload

---
+++ data/data_switch.py
@@ -0,0 +1,14 @@
+
+def data_switch(data_identifier):
+
+    if data_identifier == 'acdc':
+        from data.acdc_data import acdc_data as data_loader
+    elif data_identifier == 'lidc':
+        from data.lidc_data import lidc_data as data_loader
+    elif data_identifier == 'uzh_prostate':
+        from data.uzh_prostate_data import uzh_prostate_data as data_loader
+    else:
+        raise ValueError('Unknown data identifier: %s' % data_identifier)
+    print("数据格式是{}".format(data_identifier))
+
+    return data_loader
